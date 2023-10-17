5e0a1adcff7c789e73a284bc52fb36dbefb5c0a5
wypchlo <deron.pl.yt@gmail.com>
2023-10-17 18:18:40 +0200

    I hate js so much smh

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,40 @@
+class Vector2 {
+    x;
+    y;
+    constructor(x, y) {
+        this.x = x,
+            this.y = y;
+    }
+}
 class TxtFile {
-    static async read(path) {
-        const file = await fetch(path);
-        return await file.text();
+    path;
+    file;
+    text;
+    lines;
+    constructor(path) {
+        this.path = path;
+    }
+    static init(path) {
+        return (async function () {
+            let self = new TxtFile(path);
+            await self.set();
+            return self;
+        }());
+    }
+    async set() {
+        this.file = await fetch(this.path);
+        this.text = await this.file.text();
+        this.lines = this.text.split("\n");
+    }
+    async getText() {
+        return this.text;
+    }
+    async readLine(index) {
+        return this.lines[index];
     }
 }
 async function main() {
-    const text = await TxtFile.read("Assets/positions.txt");
-    console.log(text);
+    const positions = await TxtFile.init("Assets/positions.txt");
+    console.log(await positions.getText());
 }
 main();
